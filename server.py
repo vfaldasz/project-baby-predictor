ad49b43c97aae6cd279f3da6b42f4666057360ef
vfaldasz <vfaldasz@gmail.com>
2018-10-11 01:06:59 +0000

    registration form, uploading files complelete

--- server.py
+++ server.py
@@ -1,11 +1,21 @@
 from jinja2 import StrictUndefined
 
-from flask import (Flask, render_template, redirect, request, flash, session)
+import os
+
+from flask import (Flask, render_template, redirect, request, flash, session, url_for,send_from_directory)
 from flask_debugtoolbar import DebugToolbarExtension
+from werkzeug.utils import secure_filename
 
 from model import User, Photo, db, connect_to_db
 
+UPLOAD_FOLDER = 'upload'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
@@ -41,20 +51,20 @@ def register_process():
     email = request.form.get("email")
     password = request.form.get("password")
 
-    user_email = User.query.filter_by(email == "email")
-    if user_email == email:
-        flash("User already exists")
-        return redirect('login')
+# first, check if email is already in the database.
+# don't allow a user to create a second record with the same email.
 
-    # first, check if email is already in the database.
-    # don't allow a user to create a second record with the same email.
+    #user_email = User.query.filter_by(email = "email").first()
+    user = User.query.filter_by(email = email).first()
 
-    user_add = User(email= email, password= password)#adding user into dB
-    db.session.add(user_add)
-    db.session.commit()
-
-
-    return redirect("/")
+    if user:
+        flash("User already exists. Please log-in.")
+        return redirect('/login_form')
+    else: 
+        user_add = User(email= email, password= password)#adding user into dB
+        db.session.add(user_add)
+        db.session.commit()
+        return redirect("/new_project")
 
 @app.route("/login_form")
 def login_form():
@@ -64,16 +74,16 @@ def login_form():
 @app.route("/login_form", methods = ["POST"])
 def logged_in():
 
-    email = request.form.get("email")
+    email = request.form.get("email") #get email and password from log in form
     password = request.form.get("password")
 
-    user = User.query.filter_by(email = email).first()
+    user = User.query.filter_by(email = email).first() #query dB to see if email is already there in dB. If so, flask returns the queried data back to us as our object. The object can then be used for later use (i.e user.password)
 
     if not user:
         flash("No such user")
         return redirect("/login_form")
 
-    if user.password == password:
+    if user.password == password: 
         session['user_id']= user.user_id
         return redirect('/prior_users')
     else:
@@ -102,9 +112,35 @@ def old_projects():
 
     return render_template("results.html")
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/form-data', methods=['POST'])
+def upload_file():
     
+    #check if the post request has the file part
+    if 'file' not in request.files: #request from browser. 
+        flash('No file part')
+        return redirect('new_project')
+    file = request.files['file'] # request.file similar to request.get/.args, in this case it is requesting a file from our html. If file exists, flask returns the queried file back to us as our object. The object can then be used for later use (i.e file.filename)
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect('/new_project')
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)#make sure file is secure
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))#/uploads/name.jpeg
+        return redirect('/new_project')#place holder for now. want morphed picture to show up on results page   
+        #                         filename=filename))
 
 
+@app.route('/upload/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 
 
