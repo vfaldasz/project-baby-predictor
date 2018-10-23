90d4cce73901b3a5cb82d77591cea637cb00c65e
vfaldasz <vfaldasz@gmail.com>
2018-10-23 01:07:27 +0000

    running tests

--- model.py
+++ model.py
@@ -72,11 +72,11 @@ class Project(db.Model):
 ##############################################################################
 # Helper functions
 
-def connect_to_db(app):
+def connect_to_db(app, db_uri='postgresql:///testdb'):
     """Connect the database to our Flask app."""
 
     # Configure to use our PstgreSQL database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///babies'
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
